d5cc5c1b97994c60644a8bc221cad847196240ef
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2024-07-07 15:34:08 +0300

    chore: Update font dependencies to latest versions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		fontFamily: {
+		  sans: ['Manrope', 'sans-serif'],
+		  heading: ['"Roboto Slab"', 'serif'],
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
